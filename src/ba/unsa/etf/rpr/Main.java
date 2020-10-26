0d51bc8510316442d41ee2f645941b07e064753b
bhadzic2 <bhadzic2@etf.unsa.ba>
2020-10-26 16:41:40 +0100

    klase dodane

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -41,7 +41,8 @@ public class Main {
 //      Supermarket maksimalno sadrzi 1000 artikala. Artikl je odredjen svojim nazivom, cijenom i kodom.
 //      Radi jednostavnosti cemo pretpostaviti da su svi kodovi artikala razliciti. Program treba da radi sljedece:
 //      Kupac na pocetku dobija praznu korpu, te u nju moze ubacivati i iz nje izbacivati artikle. Ukoliko kupac zeli
-//      ubacivati u korpu potrebno je ispisati sve artikle sa kodovima, te se unosi adekvatan kod za ubacivanje u korpu. Analogno
+//      ubacivati u korpu po
+//                System.out.println("Dodavanje artikla u korpu\nArtikli u marketu su: ");trebno je ispisati sve artikle sa kodovima, te se unosi adekvatan kod za ubacivanje u korpu. Analogno
 //      za izbacivanje iz korpe je potrebno ispisati sve produkte koji se trenutno nalaze u korpi sa kodovima. Pri ubacivanju
 //      u korpu, stanje u supermarketu se mijenja i artikl vise nije dostupan. Analogno pri izbacivanju iz korpe,
 //      artikl se vraca u supermarket. Cilj je implementirati program tako da se transakcije izmedju korpe i supermarketa
@@ -60,7 +61,6 @@ public class Main {
                 return;
             }
             else if(opcija == 1){
-                System.out.println("Dodavanje artikla u korpu\nArtikli u marketu su: ");
                 ispisArtikala(supermarket.getArtikli());
                 System.out.println("Unesite kod artikla (unesite malo slovo \"c\" za odustajanje): ");
                 String kod = scanner.nextLine();
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,35 @@
 package ba.unsa.etf.rpr;
 
-public class Supermarket {
+
+public class Supermarket{
+    private Artikl[] artikli=new Artikl[1000];
+    private int br=0;
+    public void dodajArtikl(Artikl a){
+        if(br<1000){
+            artikli[br]=new Artikl(a.getNaziv(), a.getCijena(),a.getKod());
+            br++;
+        }
+    }
+    public Artikl[] getArtikli(){
+        return artikli;
+    }
+    public Artikl izbaciArtiklSaKodom(String kod){
+        for(int i=0;i<br;i++){
+            if(artikli[i].getKod().equals(kod)){
+                Artikl novi=new Artikl(artikli[i].getNaziv(), artikli[i].getCijena(),artikli[i].getKod());
+                artikli[i]=null;
+                pomjeriSve(i);
+                return novi;
+            }
+        }
+        return null;
+    }
+
+    private void pomjeriSve(int pozicija){
+        for(int i=pozicija;i<br;i++){
+            artikli[i]=artikli[i+1];
+        }
+        artikli[br]=null;
+        br--;
+    }
 }
